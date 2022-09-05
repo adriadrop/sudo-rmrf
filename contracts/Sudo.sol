c4430f78f5dbbd4d8b722186cb2cac236baff1b6
Markooor <adriadrop@gmail.com>
2022-09-05 12:47:36 +0200

    add first version of project that works in phases

--- contracts/Sudo.sol
+++ contracts/Sudo.sol
@@ -5,20 +5,32 @@ pragma solidity ^0.8.9;
 import "erc721a/contracts/ERC721A.sol";
 import "base64-sol/base64.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error ERC721Metadata__URI_QueryFor_NonExistentToken();
-error NFT_ALREADY_POWERED();
+error NFT_ALREADY_VRFED();
 error NFT_NOT_A_OWNER();
+error NFT_NEEDS_VRF();
 
-contract Sudo is ERC721A, VRFConsumerBaseV2 {
+contract Sudo is ERC721A, VRFConsumerBaseV2, Ownable {
     // Sudo NFT variables
     bool minted;
-    string private imageURIBase;
-    string private powerBarBG;
-    mapping(uint256 => uint8) powerBarValue;
-    mapping(uint256 => bool) powerBarStatus;
+    string private imageDefault;
+    string[] private imagesVRFED;
+    mapping(uint256 => uint8) vrfValue;
+    mapping(uint256 => bool) vrfStatus;
+
+    // In which phase is project currently
+    enum Phases {
+        ONE,
+        TWO,
+        THREE,
+        FOUR
+    }
+    Phases public phase;
+    strin public ipfs;
 
     // Chainlink VRF Variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
@@ -29,19 +41,19 @@ contract Sudo is ERC721A, VRFConsumerBaseV2 {
     uint32 private constant NUM_WORDS = 1;
     mapping(uint256 => uint256) public requestIdToTokenId;
 
+    // Chainlink Events
     event RandomRequested(uint256 indexed requestId, address requester);
     event RandomReceived(uint256 indexed random, uint256 indexed tokenId);
 
     constructor(
-        string memory svgBaseSliced,
-        string memory svgPowerBar,
+        string memory svg,
         address vrfCoordinatorV2,
         uint64 subscriptionId,
         bytes32 gasLane,
         uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721A("Sudo rm -rf OpenSea", "0xDEL") {
-        imageURIBase = svgBaseSliced;
-        powerBarBG = svgPowerBar;
+        imageDefault = svg;
+        ipfs = "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu";
 
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
@@ -49,17 +61,28 @@ contract Sudo is ERC721A, VRFConsumerBaseV2 {
         i_callbackGasLimit = callbackGasLimit;
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "data:application/json;base64,";
+    // Update status by passing uint into input
+    function setPhase(Phases _phase) public onlyOwner {
+        phase = _phase;
     }
 
-    function powerUp(uint256 tokenId) public payable returns (uint256 requestId) {
+    // Update ipfs
+    function setIpfs(Phases _ipfs) public onlyOwner {
+        ipfs = _ipfs;
+    }
+
+    // upload images to chain
+    function setImages(string memory _svg, uint8 position) public onlyOwner {
+        imagesVRFED[postion];
+    }
+
+    function getVRF(uint256 tokenId) public payable returns (uint256 requestId) {
         if (msg.sender != ownerOf(tokenId)) {
             revert NFT_NOT_A_OWNER();
         }
 
-        if (powerBarStatus[tokenId] == true) {
-            revert NFT_ALREADY_POWERED();
+        if (vrfStatus[tokenId] == true) {
+            revert NFT_ALREADY_VRFED();
         }
 
         requestId = i_vrfCoordinator.requestRandomWords(
@@ -78,58 +101,76 @@ contract Sudo is ERC721A, VRFConsumerBaseV2 {
         // get random and then find module of 19 so we get max 18 pixels of length
         uint8 random = uint8(randomWords[0] % 19);
         // Put random to proper tokenID
-        powerBarValue[requestIdToTokenId[requestId]] = random;
-        powerBarStatus[requestIdToTokenId[requestId]] = true;
+        vrfValue[requestIdToTokenId[requestId]] = random;
+        vrfStatus[requestIdToTokenId[requestId]] = true;
 
         emit RandomReceived(random, requestIdToTokenId[requestId]);
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        string memory imageURI;
+        // Use different code depending on phase
+        if (phase == "TWO") {
+            // here we will use onchain storage of image, same one for all
+            return tokenURI2(tokenId);
+        } else if (phase == "THREE") {
+            // here we will use different IPFS image for each NFT depending on randomness that they got
+            return tokenURI3(tokenId);
+        } else if (phase == "FOUR") {
+            // here we are full of money and we deployed onchain each image and just pull them out
+            return tokenURI4(tokenId);
+        } else {
+            return ipfs;
+        }
+    }
+
+    function tokenURI2(uint256 tokenId) public view virtual override returns (string memory) {
         string memory json;
         if (!_exists(tokenId)) {
             revert ERC721Metadata__URI_QueryFor_NonExistentToken();
         }
 
-        // We check the status and is it set to some number
-        if (powerBarStatus[tokenId]) {
-            imageURI = svgToImageURI(
-                string(
-                    abi.encodePacked(
-                        imageURIBase,
-                        powerBarBG,
-                        '<rect width="',
-                        Strings.toString(powerBarValue[tokenId]),
-                        '" height="1" x="43" y="2.5" style="fill:rgb(229,59,68);"/><rect width="',
-                        Strings.toString(powerBarValue[tokenId]),
-                        '" height="1" x="43" y="3.5" style="fill:rgb(158,40,53);"/>',
-                        "</svg>"
-                    )
-                )
-            );
-            json = string(
-                abi.encodePacked(
-                    '{"name":"',
-                    name(),
-                    '", "description":"SudoSwap taking over OpenSea", ',
-                    '"attributes": [{"trait_type": "power", "value": 100}, {"trait_type": "vanilla", "value": false}], "image":"',
-                    imageURI,
-                    '"}'
-                )
-            );
-        } else {
-            imageURI = svgToImageURI(string(abi.encodePacked(imageURIBase, "</svg>")));
-            json = string(
-                abi.encodePacked(
-                    '{"name":"',
-                    name(),
-                    '", "description":"SudoSwap taking over OpenSea", ',
-                    '"attributes": [{"trait_type": "vanilla", "value": true}], "image":"',
-                    imageURI,
-                    '"}'
-                )
-            );
+        json = string(
+            abi.encodePacked(
+                '{"name":"',
+                name(),
+                '", "description":"SudoSwap taking over OpenSea", ',
+                '"attributes": [{"trait_type": "status", "value": "original"}], "image":"',
+                imageDefault,
+                '"}'
+            )
+        );
+
+        return string(abi.encodePacked(_baseURI(), Base64.encode(bytes(json))));
+    }
+
+    function tokenURI3(uint256 _tokenId) public view override returns (string memory) {
+        if (vrfStatus[tokenId] == false) {
+            revert NFT_NEEDS_VRF();
         }
+        // Get json file with image according to random number gotten by VRF
+        return string(abi.encodePacked(ipfs, Strings.toString(vrfValue(_tokenId)), ".json"));
+    }
+
+    function tokenURI4(uint256 tokenId) public view virtual override returns (string memory) {
+        if (vrfStatus[tokenId] == false) {
+            revert NFT_NEEDS_VRF();
+        }
+
+        string memory json;
+        if (!_exists(tokenId)) {
+            revert ERC721Metadata__URI_QueryFor_NonExistentToken();
+        }
+
+        json = string(
+            abi.encodePacked(
+                '{"name":"',
+                name(),
+                '", "description":"SudoSwap taking over OpenSea", ',
+                '"attributes": [{"trait_type": "status", "value": randomized}], "image":"',
+                imagesVRFED[vrfValue(_tokenId)],
+                '"}'
+            )
+        );
 
         return string(abi.encodePacked(_baseURI(), Base64.encode(bytes(json))));
     }
@@ -138,6 +179,10 @@ contract Sudo is ERC721A, VRFConsumerBaseV2 {
         string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
         return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
     }
 
     function mint() external payable {
--- deploy/01-deploy.js
+++ deploy/01-deploy.js
@@ -14,10 +14,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // Get SVG base to pass to constructor
     const svg = fs.readFileSync("./svg/sudoswap.svg", { encoding: "utf8" });
     // Cut SVG tag from the end
-    const svgBaseSliced = svg.slice(0, -7);
+    // const svgBaseSliced = svg.slice(0, -7);
     // log(svgBaseSliced);
-    const svgPowerBar =
-        '<rect width="1" height="2" x="42" y="2.5" style="fill:rgb(235,235,235);"/><rect width="1" height="2" x="61" y="2.5" style="fill:rgb(235,235,235);"/><rect width="18" height="1" x="43" y="1.5" style="fill:rgb(235,235,235);" /><rect width="18" height="1" x="43" y="2.5" style="fill:rgb(106,198,177);" /><rect width="18" height="1" x="43" y="3.5" style="fill:rgb(97,185,165);" /><rect width="18" height="1" x="43" y="4.5" style="fill:rgb(235,235,235);"/>';
+    //const svgPowerBar =
+    '<rect width="1" height="2" x="42" y="2.5" style="fill:rgb(235,235,235);"/><rect width="1" height="2" x="61" y="2.5" style="fill:rgb(235,235,235);"/><rect width="18" height="1" x="43" y="1.5" style="fill:rgb(235,235,235);" /><rect width="18" height="1" x="43" y="2.5" style="fill:rgb(106,198,177);" /><rect width="18" height="1" x="43" y="3.5" style="fill:rgb(97,185,165);" /><rect width="18" height="1" x="43" y="4.5" style="fill:rgb(235,235,235);"/>';
 
     if (chainId == 31337) {
         // create VRFV2 Subscription
@@ -30,6 +30,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         // Our mock makes it so we don't actually have to worry about sending fund
         await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT);
     } else {
+        // Need to set this with UI or with code and then put it into config
         vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2;
         subscriptionId = networkConfig[chainId].subscriptionId;
     }
@@ -39,8 +40,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log("----------------------------------------------------");
 
     arguments = [
-        svgBaseSliced,
-        svgPowerBar,
+        svg,
         vrfCoordinatorV2Address,
         subscriptionId,
         networkConfig[chainId]["gasLane"],
