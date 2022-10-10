908cf3c643efb33be9eb425b02e1e6f72eea9648
Mark Bliss <adriadrop@gmail.com>
2022-10-10 14:30:25 +0200

    Update updateMetadata.js
    Proper update script

--- scripts/updateMetadata.js
+++ scripts/updateMetadata.js
@@ -1,16 +1,24 @@
 const fs = require("fs");
 const args = process.argv.slice(2);
-const inputFolder = args(0);
+console.log(args);
+const inputFolder = args[0];
 const dir = `${__dirname}/${inputFolder}/`;
 const inputFiles = fs.readdirSync(dir).sort();
+
+function printProgress(progress) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(progress + " updated");
+  }
 
 inputFiles.forEach((file) => {
     let id = file.split(".").shift();
     let data = JSON.parse(fs.readFileSync(`${dir}/${file}`));
 
-    data.name = `NFT Collection Name ${id}`;
-    data.image = `ipfs://sdfdfssfdfdfsdfsd/${id}.svg`;
+    data.name = `Sudo #${id}`;
+    data.image = `ipfs://QmfGJn1AT4eyityK8pBfH9LkjXiC84abmb784Nk6TnZQmD/${id}`;
 
-    fs.writeFileSync(`${dir}/${file}`, JSON.stringify(datam null, 2));
-    console.log(data);
+    printProgress(id);
+
+    fs.writeFileSync(`${dir}/${file}`, JSON.stringify(data, null, 2));
 })
